472355128d062904f0422047c67fb13496172dd2
sasaron <mac1301010162sasakir@gmail.com>
2021-12-19 11:48:53 +0900

    add RDS

--- bin/ecs-sample.ts
+++ bin/ecs-sample.ts
@@ -3,7 +3,9 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EcsSampleStack } from '../lib/ecs-sample-stack';
 import { VPCSampleStack } from '../lib/vpc-sample-stack'; 
+import { RDSSampleStack } from '../lib/rds-sample-stack';
 
 const app = new cdk.App();
 const vpcStack = new VPCSampleStack(app, 'VPCSampleStack', {});
+new RDSSampleStack(app, 'RDSSampleStack',  vpcStack.vpc, {});
 new EcsSampleStack(app, 'EcsSampleStack', vpcStack.vpc, {});
---
+++ lib/rds-sample-stack.ts
@@ -0,0 +1,31 @@
+import {
+  Stack,
+  StackProps,
+  aws_rds as rds,
+  aws_ec2 as ec2,
+  SecretValue,
+  aws_ssm as ssm
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class RDSSampleStack extends Stack {
+  public readonly rdsCluster: rds.IDatabaseCluster;
+  constructor(scope: Construct, id: string, vpc: ec2.IVpc, props?: StackProps) {
+    super(scope, id, props);
+    const user = ssm.StringParameter.valueForStringParameter(this, '/ECSSample/Dev/RDS/User');
+    const rotation = ssm.StringParameter.valueForStringParameter(this, '/ECSSample/Dev/RDS/Rotation'); // あまり良くないかも
+    const password = SecretValue.ssmSecure('/ECSSample/Dev/RDS/Password', rotation);
+    this.rdsCluster = new rds.DatabaseCluster(this, 'SampleAuroraCluster', {
+      engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_3_01_0 }),
+      credentials: rds.Credentials.fromPassword(user, password),
+
+      instanceProps: {
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MEDIUM),
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        },
+        vpc,
+      },
+    });
+  }
+}
